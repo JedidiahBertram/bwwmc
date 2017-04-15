850371cc59df8112d8c9203dfbf1bcfdc314930e
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-15 13:09:07 -0500

    completes the users seed

--- seeds/users.js
+++ seeds/users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {user_id: 'jcena', password: 'not happening', full_name: 'John Cena', phone:'488-042-8471', email:'jcena@youcantseeme.wwe', address: '8801 North Fm 620', city:'Austin', state:'Texas', zip:'78726'},
+        {user_id: 'undertaker', password: 'not happening', full_name: 'The Undertaker', phone:'341-998-2713', email:'taker@deadmanwalking.wwe', address: 'Death Valley', city:'Los Angles', state:'California', zip:'82283'},
+        {user_id: 'hulkhogan', password: 'not happening', full_name: 'Hulk Hogan', phone:'918-224-9482', email:'therealhulkhogan@hulkamaniacs.wwe', address: '8801 North Fm 620', city:'Agusta', state:'Georgia', zip:'30818'},
       ]);
     });
 };
