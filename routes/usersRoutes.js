7c0a6518c220887796d5becec7c562c51abea9f5
TOBY A GODFREY <toby@tobys-mbp.attlocal.net>
2017-04-15 11:53:27 -0500

    Updating user and showing result page

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -37,16 +37,45 @@ router.route("/edit")
     });
 
 router.route('/:user_id')
+    .get(function(req, res) {
+        //console.log('Get single user route');
+        console.log(req.params);
+
+        knex('users')
+            .where("id", req.params.user_id)
+            .then((user) => {
+                //console.log(user);
+                //res.send(user);
+                res.render('users/show', {
+                    user_id: user[0].user_id,
+                    full_name: user[0].full_name,
+                    phone: user[0].phone,
+                    email: user[0].email,
+                    address: user[0].address,
+                    city: user[0].city,
+                    state: user[0].state,
+                    zip: user[0].zip
+
+                    // user_name: user[0].password,
+                    // img_url: user[0].full_name.
+                });
+            }) //end then
+            .catch((err) => {
+                //console.log(user[0]);
+                console.log(err);
+            });
+    })
+
     .put(function(req, res) {
         console.log('In the single user PUT route for ' + JSON.stringify(req.body.user_id));
 
         knex('users')
             .where('id', '=', req.body.user.user_id)
             .update(req.body.user)
-            //.returning("id")
-            // .then(function(id) {
-            //     res.redirect(`/posts/${id}`);
-            // })
+            .returning("id")
+            .then(function(id) {
+                res.redirect(`/users/${id}`);
+            })
             .catch(function(err) {
                 console.log(err);
             });
