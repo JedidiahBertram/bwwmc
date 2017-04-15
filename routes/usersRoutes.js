c0d52b79f3d40484338e0d9e7f232a9aaa3312d2
TOBY A GODFREY <toby@tobys-mbp.attlocal.net>
2017-04-15 09:54:33 -0500

    Updating user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -24,16 +24,34 @@ router.route("/")
 
     })
 
-    .put(function(req, res) {
-
-    })
-
 //The users/new route - render the data entry page to insert a new user
 //called via http://localhost:3000/users/new
 router.route("/new")
     .get(function(req, res) {
         res.render("users/new");
     });
+
+router.route("/edit")
+    .get(function(req, res) {
+        res.render("users/edit");
+    });
+
+router.route('/:user_id')
+    .put(function(req, res) {
+        console.log('In the single user PUT route for ' + JSON.stringify(req.body.user_id));
+
+        knex('users')
+            .where('id', '=', req.body.user.user_id)
+            .update(req.body.user)
+            //.returning("id")
+            // .then(function(id) {
+            //     res.redirect(`/posts/${id}`);
+            // })
+            .catch(function(err) {
+                console.log(err);
+            });
+
+    })
 
 
 
