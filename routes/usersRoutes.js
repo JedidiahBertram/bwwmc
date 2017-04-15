93e037d99e433c951f338c2b229d797b6c3c0818
TOBY A GODFREY <toby@tobys-mbp.attlocal.net>
2017-04-15 11:57:57 -0500

    Added and remove appropriate comments

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -2,22 +2,11 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-router.route("/")
-
 
 router.route("/")
-    // .get(function(req, res) {
-    //     console.log('In the users get route');
-    //     //res.render('In the users get route');
-    // })
     .post(function(req, res) {
-        console.log('In the users post route');
-        //res.render('In the users route');
         knex('users').insert(req.body.user)
             .returning('id')
-            // .then(function(id) {
-            //     res.redirect('/users/${id}')
-            // })
             .catch(function(err) {
                 console.log(err);
             })
@@ -30,22 +19,21 @@ router.route("/new")
     .get(function(req, res) {
         res.render("users/new");
     });
-
+//Update a user route
+//Called via http://localhost:3000/users/edit
 router.route("/edit")
     .get(function(req, res) {
         res.render("users/edit");
     });
 
+//Routes specific to one user
 router.route('/:user_id')
     .get(function(req, res) {
-        //console.log('Get single user route');
         console.log(req.params);
 
         knex('users')
             .where("id", req.params.user_id)
             .then((user) => {
-                //console.log(user);
-                //res.send(user);
                 res.render('users/show', {
                     user_id: user[0].user_id,
                     full_name: user[0].full_name,
@@ -55,9 +43,6 @@ router.route('/:user_id')
                     city: user[0].city,
                     state: user[0].state,
                     zip: user[0].zip
-
-                    // user_name: user[0].password,
-                    // img_url: user[0].full_name.
                 });
             }) //end then
             .catch((err) => {
