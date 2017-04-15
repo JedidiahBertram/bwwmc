2a635beadf64decb9eb6a0bc78483f2e79cd8078
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-15 12:33:33 -0500

    completes menu_itmes seed

--- seeds/menu_items.js
+++ seeds/menu_items.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {item_name: 'Signature Chow', item_ingredients: 'Venison, Offal, Oats, Corn, Barley, Vitamin A, Vitamin E'},
+        {item_name: 'Doggie Knows Best', item_ingredients: 'Chicken, Oats, Rice, Vitamin A, Vitamin E, Vitamin D'},
+        {item_name: 'Tasty Milkbone', item_ingredients: 'Offal, Potatoes, Corn, Tyme, Vitamin C, Vitamin D'},
       ]);
     });
 };
