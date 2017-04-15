74ca03c11b7cbd6abe0b7066a23a8b9225877330
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-15 14:59:06 -0500

    adds POST and PUT routes

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -3,11 +3,36 @@ const router = express.Router();
 const knex = require('../db/knex');
 
 router.route("/")
-.post('/', (req, res) => {
-  // knex('orders')
+  .get((req, res) => {
+    knex('orders')
+    .then((orders) => {
+      res.render('orders/index', {orders})
+      // res.send('yo');
+    })
+  })
+  .post((req, res) => {
+    knex('orders').insert(req.body.order)
+      .returning('id')
+      .catch((err) => {
+        console.log(err);
+      })
 })
 
+// get route goes here
 
+.put((req, res) => {
+
+  knex('orders')
+    .where('id', '=', req.body.order.order_id)
+    .update(req.body.order)
+    .returning('id')
+    .then((id) => {
+      res.redirect(`/orders/${id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
 
 
 
