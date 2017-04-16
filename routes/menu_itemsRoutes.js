42faa402b7a81023f5e1b31a159c350995e41910
Jon Ramer <jramer87@gmail.com>
2017-04-16 14:38:17 -0500

    adds GET /menu_items route, and index.ejs view for the menu_items resource

--- routes/menu_itemsRoutes.js
+++ routes/menu_itemsRoutes.js
@@ -1,7 +1,22 @@
+"use strict";
+
+
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
 router.route("/")
+    .get((req, res) => {
+        knex('menu_items')
+            .then((items) => {
+                console.log(items);
+                res.render('menu_items/index', {
+                    items
+                })
+            });
+    });
+
+
+
 
 module.exports = router;
