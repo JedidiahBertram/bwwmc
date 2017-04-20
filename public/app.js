b0cd5e4c2919385dcec43e5381a4e6e246eb6d94
Adrian Thomas <adrian.l.thomas@outlook.com>
2017-04-20 05:48:28 -0500

    adds jquery scaffold and nav bar toggle

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
-function persistNewOrder {
+$(document).ready(() => {
+  function persistNewOrder {
+  }
+  // Make transparent navigation bar visible once the user scrolls past 200px
+  $(window).scroll(() => {
+    if($(this).scrollTop() > 200) {
+      $('.navigation').addClass('opaque');
+    }
+    $('.navigation').removeClass('opaque');
+  });
+  $(window).scroll(() => {
+    if($(this).scrollTop() > 200) {
+      $('.navigation a').addClass('opaque');
+    }
+    $('.navigation a').removeClass('opaque');
+  });
 
 
-}
+});
