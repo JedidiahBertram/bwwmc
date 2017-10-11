89b9703db390fc6ef6dc5cf6edf3dd3b8013bd7a
Jon Ramer <jramer87@gmail.com>
2017-10-11 15:32:47 -0500

    Adds comments to order_pay js file

--- public/js/order_pay.js
+++ public/js/order_pay.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
+  //Grab cart information from order and store it locally
   let order = JSON.parse(localStorage.cart);
 
+  //Loop through the local order info and repopulate the cart info
   updateCart();
 
   function updateCart() {
@@ -14,6 +16,7 @@ $(document).ready(function(){
     updateCartQuantity();
   }
 
+  //Loop through order info and sum all of the quantity properties on each item & update the cartCount property
   function updateCartQuantity() {
     let sum = null;
     for (let i = 0; i < order.length; i++) {
@@ -25,6 +28,7 @@ $(document).ready(function(){
     orderSubTotal();
   }
 
+  //Loop through the order info and sum the price * quantity for each item creating the subtotal for the cart
   function orderSubTotal() {
     $('#subTotal').append(`<td>$${subTotal}</td>`)
     let sum = 0;
@@ -42,10 +46,14 @@ $(document).ready(function(){
     }
   }
 
+  //Attach event listenter to delete glyphicon in the cart when the page loads
+  //This is necessary for the case that the user comes back to the menu page from the schedule delivery page
   $('.glyphicon-remove').click((e) => {
     removeFromCart(e);
   });
 
+  //Find the index of the order the user wants to delete and remove it from the DOM, local order,
+  //and update localStorage
   function removeFromCart(e) {
     let itemIndex = null;
     order.forEach((item) => {
@@ -61,10 +69,11 @@ $(document).ready(function(){
     });
   };
 
-  var todaysDate = new Date();
-  var date_input=$('input[name="date"]'); //our date input has the name "date"
-  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-  var options={
+  //Formatting for bootstraps date picker
+  let todaysDate = new Date();
+  let date_input=$('input[name="date"]'); //our date input has the name "date"
+  let container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  let options={
     format: 'mm/dd/yyyy',
     container: container,
     todayHighlight: true,
@@ -80,42 +89,40 @@ $(document).ready(function(){
       });
   });
 
+  //Reset the order to an empty array
+  //This should also make a post to the orders resource in the db
   $('#submitSchedule').click((e) => {
     order = [];
     localStorage.cart = JSON.stringify(order);
-    console.log(JSON.parse(localStorage.cart));
-  })
+  });
 
 
-//The action for the submit button
-//Gathers the schedule date and the status of the recurring delivery
-//checkbox and the value of the schedule dropdown
-function sendOrderToRoute(){
-    var schedDate = $('.datePickerText').val();
-    //var recurring = $('#recurringCB')[0].checked;
-    //var ddVal = $('#recurrenceDD :selected').text();
+  //Re-factor this to use the submit click event callback above
+  //Check db for formatting requirements
+  function sendOrderToRoute(){
+      var schedDate = $('.datePickerText').val();
+      //var recurring = $('#recurringCB')[0].checked;
+      //var ddVal = $('#recurrenceDD :selected').text();
 
-    if(!schedDate){
-      alert("Please schedule a delivery date");
-    }
+      if(!schedDate){
+        alert("Please schedule a delivery date");
+      }
 
-    var order = {
-      'menuItemID': JSON.parse(localStorage.cart)[0].id,
-      'quantity': JSON.parse(localStorage.cart)[0].quantity,
-      'item_price': JSON.parse(localStorage.cart)[0].item_price,
-      'schedDate': schedDate//,
-      //'recurring': recurring,
-      //'frequency': ddVal
+      var order = {
+        'menuItemID': JSON.parse(localStorage.cart)[0].id,
+        'quantity': JSON.parse(localStorage.cart)[0].quantity,
+        'item_price': JSON.parse(localStorage.cart)[0].item_price,
+        'schedDate': schedDate//,
+        //'recurring': recurring,
+        //'frequency': ddVal
+      };
+
+      //ajax call to the appropriate route
+      var request = $.ajax({
+        url: "http://localhost:3000/orders",
+        method: "POST",
+        data: order,
+        dataType: "json"
+      });
     };
-
-    console.log('view order = ', order);
-
-    //ajax call to the appropriate route
-    var request = $.ajax({
-      url: "http://localhost:3000/orders",
-      method: "POST",
-      data: order,
-      dataType: "json"
-    });
-  };
 });
