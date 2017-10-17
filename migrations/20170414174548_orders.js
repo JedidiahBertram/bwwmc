bc5f9d64e2bbf15caf1a6dc7e4495ce7ae3ed0a5
Jon Ramer <jramer87@gmail.com>
2017-10-17 15:12:02 -0500

    Updates orders seed with new fields

--- migrations/20170414174548_orders.js
+++ migrations/20170414174548_orders.js
@@ -1,50 +1,62 @@
-"use strict";
+'use strict';
 
 
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('orders', (t) => {
         t.increments()
             .primary();
-        t.string("item_1_name")
+        t.string('item_1_name')
             .notNullable();
-        t.integer("item_1_price")
+        t.integer('item_1_price')
             .notNullable();
-        t.integer("item_1_quantity")
+        t.integer('item_1_quantity')
             .notNullable();
-        t.string("item_2_name")
+        t.string('item_2_name')
             .defaultTo('');
-        t.integer("item_2_price")
+        t.integer('item_2_price')
             .defaultTo(0);
-        t.integer("item_2_quantity")
+        t.integer('item_2_quantity')
             .defaultTo(0);
-        t.string("item_3_name")
+        t.string('item_3_name')
             .defaultTo('');
-        t.integer("item_3_price")
+        t.integer('item_3_price')
             .defaultTo(0);
-        t.integer("item_3_quantity")
+        t.integer('item_3_quantity')
             .defaultTo(0);
-        t.string("item_4_name")
+        t.string('item_4_name')
             .defaultTo('');
-        t.integer("item_4_price")
+        t.integer('item_4_price')
             .defaultTo(0);
-        t.integer("item_4_quantity")
+        t.integer('item_4_quantity')
             .defaultTo(0);
-        t.string("item_5_name")
+        t.string('item_5_name')
             .defaultTo('');
-        t.integer("item_5_price")
+        t.integer('item_5_price')
             .defaultTo(0);
-        t.integer("item_5_quantity")
+        t.integer('item_5_quantity')
             .defaultTo(0);
-        t.string("item_6_name")
+        t.string('item_6_name')
             .defaultTo('');
-        t.integer("item_6_price")
+        t.integer('item_6_price')
             .defaultTo(0);
-        t.integer("item_6_quantity")
+        t.integer('item_6_quantity')
             .defaultTo(0);
-        t.integer("total")
+        t.integer('total')
             .notNullable();
         t.date('delivery_date')
         .notNullable();
+        t.string('name')
+            .notNullable();
+        t.string('email')
+            .notNullable();
+        t.string('address')
+            .notNullable();
+        t.string('city')
+            .notNullable();
+        t.string('state')
+            .notNullable();
+        t.integer('zip_code')
+            .notNullable();
     })
 };
 
