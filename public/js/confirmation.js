ffa9e0dfeb716722dcb1032f705fea4c0565548c
Jon Ramer <jramer87@gmail.com>
2017-10-27 15:40:21 -0500

    Removes ID from confirmation page temporarily

--- public/js/confirmation.js
+++ public/js/confirmation.js
@@ -2,5 +2,5 @@ $(document).ready(() => {
   "use strict";
   let order  = JSON.parse(localStorage.order);
   console.log(order);
-  $('.orderInfo').append(['<h4 class="col-xs-8 col-xs-offset-2">Order #: Insert ID here</h4>', `<h4 class="col-xs-8 col-xs-offset-2">Order date: ${new Date()}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery date: ${order.delivery_date}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery address: ${order.address} ${order.city}, ${order.state}</h4>`])
+  $('.orderInfo').append([`<h4 class="col-xs-8 col-xs-offset-2">Order date: ${new Date()}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery date: ${order.delivery_date}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery address: ${order.address} ${order.city}, ${order.state}</h4>`])
 });
