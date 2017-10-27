c4962b54311cfd93ada57986d422d9cefa3d92f0
Jon Ramer <jramer87@gmail.com>
2017-10-27 16:13:59 -0500

    Repositions content on confirmation page

--- public/js/confirmation.js
+++ public/js/confirmation.js
@@ -1,5 +1,5 @@
 $(document).ready(() => {
   "use strict";
   let order  = JSON.parse(localStorage.order);
-  $('.orderInfo').append([`<h4 class="col-xs-8 col-xs-offset-2">Order date: ${new Date()}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery date: ${order.delivery_date}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2">Delivery address: ${order.address} ${order.city}, ${order.state}</h4>`])
+  $('.orderInfo').append([`<h4 class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-3">Order date: ${new Date()}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-3">Delivery date: ${order.delivery_date}</h4>`, `<h4 class="col-xs-8 col-xs-offset-2 col-md-6 col-md-offset-3">Delivery address: ${order.address} ${order.city}, ${order.state}</h4>`])
 });
